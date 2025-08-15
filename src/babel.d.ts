ff0a534d9e1faa69007a0716b48b3884c5112680
dhrupad <drcpythonmfe@gmail.com>
2025-08-15 08:16:42 +0530

    add code

--- src/babel.d.ts
+++ src/babel.d.ts
@@ -1,19 +1,19 @@
-declare module "@babel/generator" {
-  const generator: any;
-  export default generator;
-}
+// declare module "@babel/generator" {
+//   const generator: any;
+//   export default generator;
+// }
 
-declare module "@babel/template" {
-  const template: any;
-  export default template;
-}
+// declare module "@babel/template" {
+//   const template: any;
+//   export default template;
+// }
 
-declare module "@babel/traverse" {
-  const traverse: any;
-  export default traverse;
-}
+// declare module "@babel/traverse" {
+//   const traverse: any;
+//   export default traverse;
+// }
 
-declare module "prop-types" {
-  const PropTypes: any;
-  export = PropTypes;
-}
+// declare module "prop-types" {
+//   const PropTypes: any;
+//   export = PropTypes;
+// }
